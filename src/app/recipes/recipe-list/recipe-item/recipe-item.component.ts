e5760a52390b1844ce32933d6f3cd2764d232a54
KPouianou <kosmas@visionmobile.com>
2017-12-03 16:25:49 +0200

    separate shopping list from recipes

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  // tslint:disable-next-line:no-input-rename
+  @Input() recipe: Recipe;
+  //// @Output() selectedRecipe = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.recipe);
   }
+
+  // onSelect(recipe: Recipe) {
+  //   this.selectedRecipe.emit(recipe);
+  //   console.log('Selected Recipe:' + recipe.name + ' ' + recipe.description);
+  // }
 
 }
